77972d0788674d377c34cc338ebdfcb8b87abe59
Prashanthi Bojanapu <prashanthi.bojanapu@gmail.com>
2020-10-12 14:03:20 +0530

    Update settings.py

--- login_registration/settings.py
+++ login_registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*wi&v$_lj_y9m_4^i583hb+*zdmm&mx_=c$_v*j9lk*tyaiiwj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covid-shell-coe.herokuapp.com'.'127.0.0.1']
+ALLOWED_HOSTS = ['covid-shell-coe.herokuapp.com']
 
 
 # Application definition
